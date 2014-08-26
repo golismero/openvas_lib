b868210274a4d734ef973186661c3ca2ce3f59f7
cr0hn <cr0hn@cr0hn.com>
2014-08-27 00:45:38 +0200

    syntax fixes

--- example.py
+++ example.py
@@ -1,91 +1,109 @@
-##!/usr/bin/env python
-from openvas_lib import VulnscanManager,VulnscanException
-from threading import Semaphore
-from functools import partial
-from xml.etree import ElementTree
+# !/usr/bin/env python
+
+import os
+import re
+import sys
 import base64
 import datetime
-import os
-import sys,re
 import subprocess
+from functools import partial
+from threading import Semaphore
+from xml.etree import ElementTree
 
-manager= VulnscanManager("127.0.0.1","admin","******")
-totalscan=0
-totalread=0
+from openvas_lib import VulnscanManager
+
+manager = VulnscanManager("127.0.0.1", "admin", "******")
+total_scan = 0
+total_read = 0
+
+
 def my_print_status(i):
     print str(i),
     sys.stdout.flush()
-def runCheck(host):
-    p=subprocess.Popen(["ping -c 1 "+host], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    out=p.stdout.read()
+
+
+def run_check(host):
+    p = subprocess.Popen(["ping -c 1 " + host], stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    out = p.stdout.read()
     regex = re.compile("time=\d*", re.IGNORECASE | re.MULTILINE)
     if len(regex.findall(out)) > 0:
-         return True
+        return True
     else:
-         return False
-def write_report(report_id,myip,myemail):
-    global manager
-    global totalscan 
-    Mydir="/root/myopenvas/results/"+myemail[0:myemail.find('@')]
-    if not os.path.exists(Mydir):
-        os.mkdir(Mydir)
-    fout=open(Mydir+"/"+myip+".html","wb")
-    report=manager.get_report_html(report_id)
-    fout.write(base64.b64decode(report.find("report").text))
-    fout.close()
+        return False
 
-    fout=open(Mydir+"/"+myip+".pdf","wb")
-    report=manager.get_report_pdf(report_id)
-    fout.write(base64.b64decode(report.find("report").text))
-    fout.close()
-    totalscan+=1
-    print "have scan : ",totalscan
+
+def write_report(report_id, my_ip, my_email):
+    global manager
+    global total_scan
+
+    my_dir = "/root/myopenvas/results/" + my_email[0:my_email.find('@')]
+
+    if not os.path.exists(my_dir):
+        os.mkdir(my_dir)
+
+    f_out = open(my_dir + "/" + my_ip + ".html", "wb")
+    report = manager.get_report_html(report_id)
+    f_out.write(base64.b64decode(report.find("report").text))
+    f_out.close()
+
+    f_out = open(my_dir + "/" + my_ip + ".pdf", "wb")
+    report = manager.get_report_pdf(report_id)
+    f_out.write(base64.b64decode(report.find("report").text))
+    f_out.close()
+    total_scan += 1
+    print "have scan : ", total_scan
 
 #    fout=open(Mydir+"/"+myip+".xml","w")
 #    report=manager.get_report(report_id)
 #    fout.write(ElementTree.tostring(report,encoding='utf-8',method='xml'))
 #    fout.close()
 
-def my_scan(myip,myemail):
+
+def my_scan(my_ip, my_email):
     global manager
-    Mydir="/root/myopenvas/results/"+myemail[0:myemail.find('@')]
-    if os.path.isfile(Mydir+"/"+myip+".html"):
-        print myip+" already exist"
-        return 
-    start=datetime.datetime.now()
-    print "Start of: "+myip+" at : ",start
+    my_dir = "/root/myopenvas/results/" + my_email[0:my_email.find('@')]
+
+    if os.path.isfile(my_dir + "/" + my_ip + ".html"):
+        print my_ip + " already exist"
+        return
+    start = datetime.datetime.now()
+    print "Start of: " + my_ip + " at : ", start
 #    """
-    Sem =Semaphore(0)
-    scan_id,target_id=manager.launch_scan(
-            target=myip,
-            profile="Full and fast",
-            callback_end=partial(lambda x:x.release(),Sem),
-            callback_progress=my_print_status
-            )
-    Sem.acquire()
+    sem = Semaphore(0)
+
+    scan_id, target_id = manager.launch_scan(
+        target=my_ip,
+        profile="Full and fast",
+        callback_end=partial(lambda x: x.release(), sem),
+        callback_progress=my_print_status
+    )
+    sem.acquire()
 #    """
-    end=datetime.datetime.now()
-    print "End of: "+myip+" at : ",end
+    end = datetime.datetime.now()
+    print "End of: " + my_ip + " at : ", end
     print "*******************************"
-    print "Cost :",(end-start)
+    print "Cost :", (end - start)
     print "*******************************"
 
-    report_id=manager.get_tasks_last_report_id(scan_id)
-    write_report(report_id,myip,myemail)
+    report_id = manager.get_tasks_last_report_id(scan_id)
+    write_report(report_id, my_ip, my_email)
+
+
 def my_launch_scanner():
-    global totalread
-    fp=open("last.txt","r")
-    lines =fp.readlines()
+    global total_read
+    fp = open("last.txt", "r")
+    lines = fp.readlines()
     for line in lines:
-        line=line.split(' ')
-        totalread+=1
-        print "have read : ",totalread
+        line = line.split(' ')
+        total_read += 1
+        print "have read : ", total_read
         print "========================================================================"
-        if runCheck(line[0]):
+        if run_check(line[0]):
             try:
-                my_scan(line[0],line[1])
+                my_scan(line[0], line[1])
             except:
                 print "my error found"
-                return 
+                return
         print "========================================================================"
 my_launch_scanner()
